e901c7ac15860c7e62a3a1104a5d934043b2879d
nao0725 <aqua.725.times@gmail.com>
2021-07-28 05:53:26 +0000

    Create Views

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,27 +1,37 @@
 class GroupsController < ApplicationController
-  
+
   def index
-  end 
-  
-  def show
-  end 
-  
-  def new
-  end 
-  
-  def create
-  end 
-  
-  def edit
-  end 
-  
-  def update
-  end 
-  
-  private
-  
-  def group_params
-    params.require(:group).permit(:name, user_ids: [] ) 
+    @groups = Group.all
   end
-  
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+    @group.user = current_user
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path, notice: "Created new Group!!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
+  end
+
 end
